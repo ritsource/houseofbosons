caa7a693019c0269440b7b60f8efc5f927550aa4
ritwik310 <ritwiksahaofficial@gmail.com>
2019-06-22 18:45:50 +0530

    Done for the day

--- scripts/gen-protobuf.sh
+++ scripts/gen-protobuf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-protoc --proto_path=. --js_out=./lib/protobuf --go_out=. ./lib/protobuf/admin.proto
-protoc --proto_path=. --js_out=./lib/protobuf --go_out=. ./lib/protobuf/blog.proto
-protoc --proto_path=. --js_out=./lib/protobuf --go_out=. ./lib/protobuf/category.proto
+protoc --proto_path=. --go_out=. ./lib/protobuf/admin/admin.proto
+protoc --proto_path=. --go_out=. ./lib/protobuf/blog/blog.proto
